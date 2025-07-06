875d3e6a45856478451e0d8ba2151dd3f5e4d255
ALFATELECOM <149483794+ALFATELECOM@users.noreply.github.com>
2025-07-06 23:41:14 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,1 +1,10 @@
-// placeholder content
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  }
+}
+
+module.exports = nextConfig
